08f4a4365b7327350b60ad8c81fb61727ae9d2e0
tokumaM <tokuma.business@outlook.jp>
2021-07-06 12:54:25 +0900

    ループ処理に対応

--- ForStudy/Program.cs
+++ ForStudy/Program.cs
@@ -1,20 +1,34 @@
 ﻿using System;
 using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace ForStudy
 {
     class Program
     { 
-        static void Main()
+        static async Task Main()
         {
             string apiKey = "95712e3e136a6e7457e6fc6502f0afe3";
-            string jsonString = ControllWeatherAPI.Fetch(apiKey);
-            JsonElement collection = ControllWeatherAPI.Deserialize(jsonString);
-            string datePath = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
-            string filePath = datePath + ".txt";
-            string directoryPath = @"C:\Users\user\weather\current\" + datePath;
-            ControllWeatherAPI.Save(collection, filePath, directoryPath);
-            Console.WriteLine("終了");
+            ControllWeatherAPI controllWeather = new ControllWeatherAPI();
+            bool checkContinuing;
+            //Mainメソッドでループしているが、ControllWeatherAPI.cs内でTaskをコレクションして、一定数たまったら、
+            //LINQでラムダ式に対してタスク継続処理すればよい
+
+            do
+            {
+                Task<string> jsonString = controllWeather.Fetch(apiKey);
+                JsonElement collection = ControllWeatherAPI.Deserialize(jsonString.Result);
+                string datePath = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
+                string filePath = datePath + ".txt";
+                string directoryPath = @"C:\Users\user\weather\current\" + datePath;
+                ControllWeatherAPI.Save(collection, filePath, directoryPath);
+                Console.WriteLine("終了");
+                //あと何回続けるか、にしたい
+                Console.WriteLine("続けますか Yes, No:");
+                string readString = Console.ReadLine();
+                checkContinuing = readString == "Yes" ? true : false;
+                await Task.Delay(1000);
+            } while (checkContinuing);
         
             Console.ReadLine();
         }
