ca9138cd882f42947114da3640d0998d07a6ff26
tokumaM <tokuma.business@outlook.jp>
2021-07-07 13:59:07 +0900

    高速化達成

--- ForStudy/ControllWeatherAPI.cs
+++ ForStudy/ControllWeatherAPI.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.IO;
 using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Net;
+using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 
@@ -11,52 +12,51 @@ namespace ForStudy
 {
     internal class ControllWeatherAPI
     {
-        internal static async Task<Dictionary<DateTime, string>> Fetch(string apiKey)
+        public string apiKey { get; set; }
+
+        internal async Task<Dictionary<DateTime, string>> FetchAsync()
         {
             string uri = "https://api.openweathermap.org/data/2.5/weather?id=1850147&units=metric&lang=ja&appid=" + apiKey;
             Console.WriteLine("何回取得しますか?");
             int count = int.Parse(Console.ReadLine());
             IEnumerable<int> countArray = Enumerable.Range(0, count);
-            string jsonString = default(string);
             Dictionary<DateTime, string> jsonDict = new Dictionary<DateTime, string>();
             Stopwatch stopwatch = new Stopwatch();
-            try
+            foreach (var index in countArray)
             {
-                foreach (var index in countArray)
+                WebRequest myWebRequest = WebRequest.Create(uri);
+                DateTime dateTime = DateTime.Now;
+                stopwatch.Start();
+                Task<WebResponse> myHttpWebResponseTask = myWebRequest.GetResponseAsync();
+                int taskId = myHttpWebResponseTask.Id;
+                WebResponse myHttpWebResponse = await myHttpWebResponseTask;
+                stopwatch.Stop();
+                TimeSpan timeSpan = stopwatch.Elapsed;
+                Console.WriteLine($"ダウンロード経過時間: {timeSpan.Milliseconds}msec");
+                Console.WriteLine($"task number {myHttpWebResponseTask.Id} is done.");
+                Console.WriteLine("ダウンロード完了");
+                Stream myHttpWebResponseStream = myHttpWebResponse.GetResponseStream();
+                using (var streamReader = new StreamReader(myHttpWebResponseStream))
                 {
-                    WebRequest myWebRequest = WebRequest.Create(uri);
-                    DateTime dateTime = DateTime.Now;
-                    stopwatch.Start();
-                    Task<WebResponse> myHttpWebResponseTask = myWebRequest.GetResponseAsync();
-                    int taskId = myHttpWebResponseTask.Id;
-                    WebResponse myHttpWebResponse = await myHttpWebResponseTask;
-                    stopwatch.Stop();
-                    TimeSpan timeSpan = stopwatch.Elapsed;
-                    Console.WriteLine($"ダウンロード経過時間: {timeSpan.Milliseconds}msec");
-                    Console.WriteLine($"task number {myHttpWebResponseTask.Id} is done.");
-                    Console.WriteLine("ダウンロード完了");
-                    Stream myHttpWebResponseStream = myHttpWebResponse.GetResponseStream();
-                    using (var streamReader = new StreamReader(myHttpWebResponseStream))
-                    {
-                        jsonString = streamReader.ReadToEnd();
-                    }
-                    myHttpWebResponse.Close();
-                    await Task.Delay(1000);
+                    string jsonString = streamReader.ReadToEnd();
                     jsonDict[dateTime] = jsonString;
                 }
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
+                myHttpWebResponse.Close();
+                await Task.Delay(1000);
             }
             return jsonDict;
         }
 
-        internal static JsonElement Deserialize(string jsonString)
+        internal async Task<JsonElement> DeserializeAsync(string jsonString)
         {
-            using (JsonDocument document = JsonDocument.Parse(jsonString))
+            Encoding encoding = Encoding.GetEncoding("utf-8");
+            using (MemoryStream memoryStream = new MemoryStream(encoding.GetBytes(jsonString)))
             {
-                return document.RootElement.Clone();
+                using (Task<JsonDocument> documentTask = JsonDocument.ParseAsync(memoryStream))
+                {
+                    JsonDocument document = await documentTask;
+                    return document.RootElement.Clone();
+                }
             }
         }
 
@@ -70,10 +70,10 @@ namespace ForStudy
             {
                 foreach (JsonElement weather in weatherCollection)
                 {
+                    TakeWeatherIcon(weather);
                     sw.WriteLine("id:" + weather.GetProperty("id"));
                     sw.WriteLine("main:" + weather.GetProperty("main"));
                     sw.WriteLine("description:" + weather.GetProperty("description"));
-                    TakeWeatherIcon(weather);
                 }
             }
 
@@ -82,8 +82,9 @@ namespace ForStudy
                 string weatherIcon = weather.GetProperty("icon").ToString();
                 string donloadOption = "2x.png";
                 string uri = @"https://openweathermap.org/img/wn/" + weatherIcon + '@' + donloadOption;
+                Uri uri1 = new Uri(uri);
                 WebClient webClient = new WebClient();
-                webClient.DownloadFile(uri, donloadOption);
+                webClient.DownloadFileAsync(uri1, donloadOption);
             }
         }
     }
--- ForStudy/Program.cs
+++ ForStudy/Program.cs
@@ -1,26 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace ForStudy
 {
     class Program
     {
-        static async Task Main()
+        static void Main()
         {
-            string apiKey = "95712e3e136a6e7457e6fc6502f0afe3";
-            Dictionary<DateTime, string> keyValuePairs = ControllWeatherAPI.Fetch(apiKey).Result;
-            foreach (KeyValuePair<DateTime, string> keyValuePair in keyValuePairs)
+            ControllWeatherAPI controllWeatherAPI = new ControllWeatherAPI();
+            controllWeatherAPI.apiKey = "95712e3e136a6e7457e6fc6502f0afe3";
+            Task<Dictionary<DateTime, string>> keyValuePairs = controllWeatherAPI.FetchAsync();
+            foreach (KeyValuePair<DateTime, string> keyValuePair in keyValuePairs.Result)
             {
                 DateTime dateTime = keyValuePair.Key;
                 string jsonString = keyValuePair.Value;
                 string datePath = dateTime.ToString("yyyy-MM-dd-HH-mm-ss");
                 string directoryPath = @"C:\Users\user\weather\current\" + datePath;
                 string filePath = datePath + ".txt";
-                JsonElement jsonElement = ControllWeatherAPI.Deserialize(jsonString);
-                ControllWeatherAPI.Save(jsonElement, filePath, directoryPath);
+                Task<JsonElement> jsonElementTask = controllWeatherAPI.DeserializeAsync(jsonString);
+                ControllWeatherAPI.Save(jsonElementTask.Result, filePath, directoryPath);
             }
             Console.WriteLine("作業終了");
             Console.ReadLine();
