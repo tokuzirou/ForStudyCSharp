d9bf4528ef846ab9e4bfe68c5eb696db1b2aeff6
tokumaM <tokuma.business@outlook.jp>
2021-07-14 11:35:48 +0900

    Command型のイベントハンドラー登録用の拡張メソッド

---
+++ ForStudyOperatingBlowser/CommandExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+
+namespace ForStudyOperatingBlowser
+{
+    static class CommandExtensions
+    {
+        static RootCommand ConfigureFromMethod<T>(this RootCommand command, string methodName)
+        {
+            Command commandObject = registerHandler<T>(command, methodName);
+            bool check = commandObject is RootCommand rootCommand;
+            try
+            {
+                if (!check)
+                    throw new Exception("型の不一致");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                rootCommand = commandObject as RootCommand;
+            }
+            return rootCommand;
+        }
+
+        static Command ConfigureFromMethod<T>(this Command command, string methodName)
+        {
+            return registerHandler<T>(command, methodName);
+        }
+
+        private static Command registerHandler<T>(Command command, string methodName)
+        {
+            MethodInfo methodInfo = typeof(T).GetMethod(methodName, BindingFlags.Default);
+            command.Handler = CommandHandler.Create(methodInfo);
+            return command;
+        }
+    }
+}
